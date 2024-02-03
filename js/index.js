32fc0d0008b507426d9cd49761a0356228f27da0
Lucas Gest <74868832+LucasGest@users.noreply.github.com>
2024-02-03 15:18:09 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let btnRooster = document.querySelector(".block p a");
+
+btnRooster.addEventListener("keypress", function (e) {
+  if (e.key === "R") {
+    console.log("jsqvxwfbvfhvb");
+    document.location.href = "../menu/rooster.html";
+  }
+});
